0703007a256a5af03d76a6badb0fb831d9b0883a
Ocelots-etc <catherinedgomez@gmail.com>
2020-08-26 02:21:05 -0500

    fellow students cli project assist

--- lib/api.rb
+++ lib/api.rb
@@ -1,13 +1,14 @@
+require_relative '../config/key'
+
 class API
 
   def self.jp_restaurants
-    restaurants = RestClient.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=japanese+restaurants+in+NewJersey&key=#{key}")
-    restaurant_hash = JSON.parse(restaurant)
-    restaurants.each_with_index do |restaurant, n|
-            
-      # description = self.scrub_html(bookshelf_hash[:volumeInfo][:description])
-      # description = self.wrap_text(description)
-      Restaurants.new(restaurant_hash["name"], restaurant_hash["formatted_address"], restaurant_hash["opening_hours"], restaurant_hash["rating"], restaurant_hash["user_ratings_total"])
+    restaurants = RestClient.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=japanese+restaurants+in+NewJersey&key=#{Key.access_key}")
+    restaurants_hash = JSON.parse(restaurants)
+    restaurants_list = (restaurants_hash["results"][0]["ChIJq2QlUzjJxokR7N0xUrdPT4Q0"], restaurants_hash["results"][0]["ChIJtzzjSqHNxokRbhEj_3fc9XU"])  
+    restaurants_hash.each_with_index do |restaurant, n|
+      Restaurants.new(restaurants_hash["results"][0]["name"], restaurants_hash["results"][0]["formatted_address"], restaurants_hash["results"][0]["opening_hours"], restaurants_hash["results"][0]["rating"], restaurants_hash["results"][0]["user_ratings_total"], restaurants_hash["results"][0]["place_id"])
     end
-  end  
+  end
+
 end
--- lib/restaurants.rb
+++ lib/restaurants.rb
@@ -1,17 +1,19 @@
 class Restaurants
 
-  attr_accessor :name, :formatted_address, :opening_hours, :rating, :user_ratings_total 
+  attr_accessor :name, :formatted_address, :opening_hours, :rating, :user_ratings_total, :place_id
   # through a reader and a writer by using attr_accessor to make the variables available outside of the class
 
   @@all = []  # the class variable array
 
   #------------------------------------------------------------------------------------
-  def initialize(name, formatted_address, opening_hours, rating, user_ratings_total)  # this method creates a new instances of a
+  def initialize(name, formatted_address, opening_hours, rating, user_ratings_total, place_id) #, formatted_address, opening_hours, rating, user_ratings_total)  # this method creates a new instances of a
+
     @name = name  # book from the API output and assigns the attributes to instance variables
     @formatted_address = formatted_address
     @opening_hours = opening_hours
     @rating = rating
     @user_ratings_total = user_ratings_total
+    @place_id = place_id
     @@all << self  # stores each and every instance of a book in the class variable array
   end
   #------------------------------------------------------------------------------------
